903c075f7bcebc949890755a9e8e26c068890dc1
hiro9108 <hiroshi.8.egawa@gmail.com>
2021-03-18 18:04:37 -0700

    change: model

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import Home, accessUserStatuses, accessCompanies
+from .views import Home, accessAllUserStatuses, getUserStatus, accessAllCompanies, getCompany
 
 urlpatterns = [
     path('', Home),
-    path('user_status/', accessUserStatuses),
-    path('company/', accessCompanies),
+    path('user_status/', accessAllUserStatuses),
+    path('user_status/<str:pk>/', getUserStatus),
+    path('company/', accessAllCompanies),
+    path('company/<str:pk>/', getCompany),
 ]
